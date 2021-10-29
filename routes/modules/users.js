247860ac707df6007c975c80f59b87b01506a096
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-10-29 16:18:45 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,16 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-  res.render('login');
-})
+const User = require("../../models/user");
 
-router.post('/login', (req, res) => {
-  res.render('login');
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
-router.get('/register', (req, res) => {
-  res.render('register');
-})
+router.post("/login", (req, res) => {
+  res.render("login");
+});
 
-module.exports = router
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      console.log("User already exist.");
+      res.render("register", {
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    } else {
+      return User.create({
+        name,
+        email,
+        password,
+      })
+
+        .then(() => res.redirect("/"))
+        .catch((err) => console.log(err));
+
+      // const newUser = new User({
+      //   name,
+      //   email,
+      //   password,
+      // });
+      // newUser
+      //   .save()
+      //   .then(() => res.redirect("/"))
+      //   .catch((err) => console.log(err));
+    }
+  });
+});
+
+module.exports = router;
