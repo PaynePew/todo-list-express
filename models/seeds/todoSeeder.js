faf65bd140ce8d8341742c462bef497e68572eec
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-03 11:10:26 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,39 @@
-const Todo = require('../todo')
-const db = require('../../config/mongoose')
+const bcrypt = require("bcryptjs");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
+const Todo = require("../todo");
+const User = require("../user");
+const db = require("../../config/mongoose");
 
-  console.log('done')
-})
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678",
+};
+
+db.once("open", () => {
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: 10 }, (_, index) => {
+          return Todo.create({ name: `name-${index}`, userId });
+        })
+      );
+    })
+    .then(() => {
+      console.log("done.");
+      process.exit();
+    });
+});
